8599959726dbf18d8226938d3e67a4b3120fb069
sar0868 <safarov-ar@yandex.ru>
2024-09-25 16:06:55 +0300

    hw7

--- homework_7/scriptHW7.js
+++ homework_7/scriptHW7.js
@@ -1,2 +1,33 @@
-const el = document.querySelector("li:nth-child(2)");
-el.style.color = "green";
+const elInput = document.querySelector(".inputText");
+const button = document.querySelector("#button");
+const divBox = document.querySelector(".text");
+
+button.hidden = true;
+elInput.oninput = function () {
+  newText = elInput.value;
+  button.hidden = false;
+};
+
+function buttonClick() {
+  if (divBox.childElementCount == 4) {
+    divBox.removeChild(divBox.firstChild);
+  }
+  addElement(newText);
+  clearInput();
+  button.hidden = true;
+}
+
+function addElement(text) {
+  const newEl = document.createElement("p");
+  newEl.innerText = text;
+  divBox.append(newEl);
+}
+
+function clearInput() {
+  elInput.value = "";
+}
+
+// function afterInputText() {
+//   newText = elInput.value;
+//   button.hidden = false;
+// }
