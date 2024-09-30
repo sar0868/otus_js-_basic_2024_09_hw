a56d726decc326c4e5fae53f2e4f8d169792b31a
sar0868 <safarov-ar@yandex.ru>
2024-09-30 14:18:55 +0300

    hw8

--- homework_8/scriptHW8.js
+++ homework_8/scriptHW8.js
@@ -1,10 +1,9 @@
 // 1
-const inputDate = prompt("Input date ДД.ММ.ГГГГ");
-
-const [day, month, year] = inputDate.split(".");
-
-const date = new Date(`${year}-${month}-${day}`);
-console.log(inputDate, week(date.getDay()));
+function createDate(inputStr) {
+  const [day, month, year] = inputStr.split(".");
+  const date = new Date(`${year}-${month}-${day}`);
+  return date;
+}
 
 function week(day) {
   const week = [
@@ -19,6 +18,10 @@ function week(day) {
   return week[day - 1];
 }
 
+const inputDate = prompt("Input date ДД.ММ.ГГГГ");
+const inpDate = createDate(inputDate);
+console.log(inputDate, week(inpDate.getDay()));
+
 // 2
 const now = new Date();
 
@@ -26,3 +29,18 @@ const hours = now.getHours();
 const minutes = now.getMinutes();
 const minPass = hours * 60 + minutes;
 console.log(`С начала сегодняшнего дня прошло ${minPass} минут.`);
+
+// 3
+
+const birthdayUser1 = "08.11.1968";
+const bdUser1 = createDate(birthdayUser1);
+const birthdayUser2 = "09.11.1968";
+const bdUser2 = createDate(birthdayUser2);
+
+if (bdUser1 < bdUser2) {
+  console.log("user2 моложе user1");
+} else if (bdUser1 > bdUser2) {
+  console.log("user1 моложе user2");
+} else {
+  console.log("user1 и user2 родились в один день");
+}
