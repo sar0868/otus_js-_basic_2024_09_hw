4ff04f4936a2ed9b715946b2153773ef7035d533
sar0868 <safarov-ar@yandex.ru>
2024-11-15 14:44:40 +0300

    test for hw1

--- homework_1/scriptHW1.js
+++ homework_1/scriptHW1.js
@@ -1,18 +1,43 @@
-const a = +prompt("Input number a: ");
-const b = +prompt("Input number b: ");
-console.log(a + b);
-
-const txt1 = prompt("Input text: ");
-const txt2 = prompt("Input text: ");
-
-const sumLengthTexts = txt1.length + txt2.length;
-console.log(
-  `Количество символов в строках ${txt1} и ${txt2} = ${sumLengthTexts}`
-);
-
-const number = prompt("Введите трехзначное число: ");
-let sum = 0;
-for (let i = 0; i < number.length; i++) {
-  sum += Number(number[i]);
+export function sum(a, b) {
+  console.log(a + b);
+  return a + b;
 }
-console.log(`Сумма цифр введеного числа ${number} равна ${sum}`);
+
+export function mult(a, b) {
+  console.log(a * b);
+  return a * b;
+}
+
+export function totalChar(str1, str2) {
+  const sumLengthTexts = str1.length + str2.length;
+  console.log(
+    `Количество символов в строках ${str1} и ${str22} = ${sumLengthTexts}`
+  );
+  return sumLengthTexts;
+}
+
+function sumNumbers(number) {
+  let sum = 0;
+  for (let i = 0; i < number.length; i++) {
+    sum += Number(number[i]);
+  }
+  console.log(`Сумма цифр введеного числа ${number} равна ${sum}`);
+  return sum;
+}
+
+// const txt1 = prompt("Input text: ");
+// const txt2 = prompt("Input text: ");
+
+// const sumLengthTexts = txt1.length + txt2.length;
+// console.log(
+//   `Количество символов в строках ${txt1} и ${txt2} = ${sumLengthTexts}`
+// );
+
+// const number = prompt("Введите трехзначное число: ");
+// let sum = 0;
+// for (let i = 0; i < number.length; i++) {
+//   sum += Number(number[i]);
+// }
+// console.log(`Сумма цифр введеного числа ${number} равна ${sum}`);
+
+// module.exports = [sum, mult, totalChar, sumNumbers];
