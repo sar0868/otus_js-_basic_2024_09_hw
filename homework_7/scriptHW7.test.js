cfcad824a700af9493d3fab0fb341e1fb72b405e
sar0868 <safarov-ar@yandex.ru>
2024-11-19 16:13:02 +0300

    hw07 in progress

--- homework_7/scriptHW7.test.js
+++ homework_7/scriptHW7.test.js
@@ -5,47 +5,43 @@
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 const dom = new JSDOM(
-  `<body>
-    <input
-      type="text"
-      class="inputText"
-      placeholder="Введите текст параграфа"
-    />
-    <button id="button">Добавить параграф</button>
-    <div class="text">
-      <p>1</p>
-      <p>2</p>
-      <p>3</p>
-    </div>
-    </body> `
+  `
+  <!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>Document</title>
+    </head>
+    <body>
+      <input
+        type="text"
+        class="inputText"
+        placeholder="Введите текст параграфа"
+      />
+      <button id="button">Добавить параграф</button>
+      <div class="text">
+        <p>1</p>
+        <p>2</p>
+        <p>3</p>
+      </div>
+      <script src="./scriptHW7.js"></script>
+    </body>
+  </html>`
 );
 global.document = dom.window.document;
 global.window = dom.window;
 
 import { buttonClick, addElement } from "./scriptHW7";
 
-// jest.mock("./__mocks__/document.js");
 describe("tests hw 7", () => {
   //   beforeAll(() => {
   //     Object.defineProperty(global, "document", {});
   //   });
   //   jest.mock("document");
-  it.skip("should button hide", () => {
-    // const result = button.hidden;
-    // expect(result).toBe(true);
-  });
+  it.todo("should button hide");
 
-  it.todo("should added new p insert input text", () => {
-    const elInput = document.querySelector(".text p");
-    // elInput.value = "hello";
-    console.log(elInput.textContent);
-    // const el = document.querySelector(".text p");
-    // const result = addElement(newText);
-    // const getText = el.textContent;
-    // const getText = elInput.value;
-
-    // expect(getText).toBe(1);
-  });
+  it.todo("should added new p insert input text");
 
   it.todo("should count p equals 5, new p always first.");
 });
