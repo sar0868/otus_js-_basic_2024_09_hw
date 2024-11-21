62ea45084493a2fb1ccc5915f5526f96ca68d9da
sar0868 <safarov-ar@yandex.ru>
2024-11-21 10:45:40 +0300

    hw07  in progress

--- homework_7/page.js
+++ homework_7/page.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const { JSDOM } = require("jsdom");
 
-const html = fs.readFileSync("./index.html");
+const html = fs.readFileSync("./indexHW7.html");
 const page = new JSDOM(html);
 
 module.exports = page;
--- homework_7/scriptHW7.test.js
+++ homework_7/scriptHW7.test.js
@@ -5,35 +5,37 @@
 // const fs = require("fs");
 // global.window.document.html.innerHTML = fs.readFileSync("./index.html");
 
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
-const dom = new JSDOM(
-  `
-    <body>
-      <input
-        type="text"
-        class="inputText"
-        placeholder="Введите текст параграфа"
-      />
-      <button id="button">Добавить параграф</button>
-      <div class="text">
-        <p>1</p>
-        <p>2</p>
-        <p>3</p>
-      </div>
-    </body>`
-);
-global.document = dom.window.document;
-global.window = dom.window;
+// const jsdom = require("jsdom");
+// const { JSDOM } = jsdom;
+// const dom = new JSDOM(
+//   `
+//     <body>
+//       <input
+//         type="text"
+//         class="inputText"
+//         placeholder="Введите текст параграфа"
+//       />
+//       <button id="button">Добавить параграф</button>
+//       <div class="text">
+//         <p>1</p>
+//         <p>2</p>
+//         <p>3</p>
+//       </div>
+//     </body>`
+// );
+// global.document = dom.window.document;
+// global.window = dom.window;
 
-import { buttonClick, addElement } from "./scriptHW7";
+// import { buttonClick, addElement } from "./scriptHW7";
 
 describe("tests hw 7", () => {
   //   beforeAll(() => {
   //     Object.defineProperty(global, "document", {});
   //   });
   //   jest.mock("document");
-  it.todo("should button hide");
+  it.todo("should button hide", ()=>{
+    const 
+  });
 
   it.todo("should added new p insert input text");
 
